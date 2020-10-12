fe233db797392c8e8b2786ea3b446789bd2b20eb
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-10-12 16:38:11 +0500

    Updates

---
+++ precache-manifest.efc836848667d8dd601a21fc4165529e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54123f9b263c07dd7bfcc922985910b2",
+    "url": "/identity/index.html"
+  },
+  {
+    "revision": "9d57ce5ed888864234a9",
+    "url": "/identity/static/css/main.699e92a4.chunk.css"
+  },
+  {
+    "revision": "68e07984439c9959c0d5",
+    "url": "/identity/static/js/2.516e4ac0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/identity/static/js/2.516e4ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d57ce5ed888864234a9",
+    "url": "/identity/static/js/main.2b9cb460.chunk.js"
+  },
+  {
+    "revision": "ce50df2301005f1e48cf",
+    "url": "/identity/static/js/runtime-main.a855a76a.js"
+  }
+]);
