626c3fb5a6c40f34b95d92091cc47ef114c3c10d
UbaidurReham1 <ubaidshan007@gmail.com>
2020-11-10 21:09:51 +0500

    - #121

--- src/hoc/editForm/EditForm.js
+++ src/hoc/editForm/EditForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Forms from 'containers/Forms/Forms';
 
-const EditForm = () => {
-    return <Forms isEdit={true}/>;
+const EditForm = (props) => {
+    return <Forms isEdit={true} {...props}/>;
 }
 export default EditForm;
