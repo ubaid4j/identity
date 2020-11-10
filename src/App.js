00580d93163a9f08fca4f0cff766073e0c5f662e
UbaidurReham1 <ubaidshan007@gmail.com>
2020-11-10 20:18:45 +0500

    - #130

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import React, {Suspense} from 'react';
-import './App.css';
-import Layout from "./hoc/Layout/Layout";
-import Forms from "./containers/Forms/Forms";
-import {Redirect, Route, Switch} from "react-router";
-import Preview from "./containers/preview/Preview";
-import UserProvider from "./providers/UserProvider";
-import GuardedRoute from "./hoc/gaurdedRoute/GaurdedRoute";
-import Home from "./components/home/Home";
-import Login from "./containers/login/Login";
-import SignUp from "./containers/signup/SignUp";
-import Welcome from "./components/welcome/Welcome";
-import Logout from "./containers/logout/Logout";
-import Congrats from "./components/congrats/Congrats";
+import 'App.css';
+import Layout from 'hoc/Layout/Layout';
+import Forms from 'containers/Forms/Forms';
+import {Redirect, Route, Switch} from 'react-router';
+import Preview from 'containers/preview/Preview';
+import UserProvider from 'providers/UserProvider';
+import GuardedRoute from 'hoc/gaurdedRoute/GaurdedRoute';
+import Home from 'components/home/Home';
+import Login from 'containers/login/Login';
+import SignUp from 'containers/signup/SignUp';
+import Welcome from 'components/welcome/Welcome';
+import Logout from 'containers/logout/Logout';
+import Congrats from 'components/congrats/Congrats';
+import EditForm from 'hoc/editForm/EditForm';
 
-function App() {
+const App = () => {
 
     let routes = (
         <Switch>
             <Suspense fallback={<div>Loading ....</div>}>
-                <GuardedRoute path="/identity/create" exact component={Forms} />
-                <GuardedRoute path="/identity/preview" exact component={Preview} />
-                <GuardedRoute path="/identity/welcome" exact component={Welcome} />
-                <GuardedRoute path={"/logout"} exact component={Logout} />
-                <GuardedRoute path="/identity/congrats" component={Congrats}/>
-                <Route path="/identity/signup" exact component={SignUp}/>
-                <Route path="/identity/login" exact component={Login}/>
-                <Route path="/identity" exact component={Home}/>
-                <Route render={() => <Redirect to="/identity" />}/>
-                {/*<Redirect to="/identity/preview" />*/}
+                <GuardedRoute path='/identity/create' exact component={Forms} />
+                <GuardedRoute path='/identity/edit' exact component={EditForm} />
+                <GuardedRoute path='/identity/preview' exact component={Preview} />
+                <GuardedRoute path='/identity/welcome' exact component={Welcome} />
+                <GuardedRoute path={'/logout'} exact component={Logout} />
+                <GuardedRoute path='/identity/congrats' component={Congrats}/>
+                <Route path='/identity/signup' exact component={SignUp}/>
+                <Route path='/identity/login' exact component={Login}/>
+                <Route path='/identity' exact component={Home}/>
+                <Route render={() => <Redirect to='/identity' />}/>
             </Suspense>
         </Switch>
     );
 
     return (
-        <div className="App">
+        <div className='App'>
             <Layout>
                 <UserProvider>
                     {routes}
